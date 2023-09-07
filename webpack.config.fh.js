a49e18353676b5cbb296fb30813243f286a4d98f
Guna <ksguna1992@gmail.com>
2023-09-07 14:47:27 +0530

    Flavours added for fh

---
+++ webpack.config.fh.js
@@ -0,0 +1,109 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const appDirectory = path.resolve(__dirname);
+const { presets, plugins } = require(`${appDirectory}/babel.config.js`);
+const compileNodeModules = [
+  // Add every react-native package that needs compiling
+  'react-native-web-webview',
+  'react-native-webview',
+].map((moduleName) => path.resolve(appDirectory, `node_modules/${moduleName}`));
+const ReactWebConfig = require('react-web-config/lib/ReactWebConfig').ReactWebConfig;
+const envFilePath = path.resolve(__dirname, '.env.fh');
+
+const babelLoaderConfiguration = {
+  test: /\.(js|jsx|ts|tsx)$/, // Updated to include .jsx
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(__dirname, "index.web.js"), // Entry to your application
+    path.resolve(__dirname, "App.tsx"), // Updated to .jsx
+    path.resolve(__dirname, "src"),
+    path.resolve(__dirname, "component"),
+    ...compileNodeModules,
+  ],
+  use: {
+    loader: "babel-loader",
+    options: {
+      cacheDirectory: true,
+      presets,
+      plugins,
+    },
+  },
+};
+
+const svgLoaderConfiguration = {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: "@svgr/webpack",
+    },
+  ],
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: "url-loader",
+    options: {
+      name: "[name].[ext]",
+    },
+  },
+};
+
+const tsLoaderConfiguration = {
+  test: /\.(ts|tsx)$/,
+  include: [
+    path.resolve(__dirname, "src"), // Adjust this to your source code directory
+  ],
+  use: "ts-loader",
+};
+
+const webViewConfig = {
+  test: /postMock.html$/,
+  use: {
+    loader: 'file-loader',
+    options: {
+      name: '[name].[ext]'
+    }
+  }
+};
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, "index.web.js"),
+  },
+  output: {
+    path: path.resolve(appDirectory, "dist"),
+    publicPath: "/",
+    filename: "rnw.bundle.js",
+  },
+  resolve: {
+    extensions: [".web.tsx", ".web.ts", ".tsx", ".ts", ".web.js", ".js"],
+    alias: {
+      "react-native$": "react-native-web",
+      'react-native-webview': 'react-native-web-webview',
+      'react-native-config': 'react-web-config'
+    },
+  },
+  module: {
+    rules: [
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      svgLoaderConfiguration,
+      tsLoaderConfiguration,
+      webViewConfig
+    ],
+  },
+  plugins: [
+    ReactWebConfig(envFilePath),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "index.html"),
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(true),
+    }),
+  ],
+};
