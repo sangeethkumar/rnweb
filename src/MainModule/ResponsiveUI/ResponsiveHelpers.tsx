583209306a084663c7230bc26b5d115aa349b799
sangeethkumar.v@foodhub.com <lvs.sangeeth@gmail.com>
2023-09-13 16:02:20 +0100

    Update ResponsiveHelpers.tsx

--- src/MainModule/ResponsiveUI/ResponsiveHelpers.tsx
+++ src/MainModule/ResponsiveUI/ResponsiveHelpers.tsx
@@ -10,52 +10,59 @@ export const isLandscapeDevice = deviceWidth >= deviceHeight;
 
 // Determine the device mode based on screen width
 export const getDeviceMode = () => {
-    return isLandscapeDevice
-        ? DIMENSION_MODES.IS_TABLET_LANDSCAPE_MODE
-        : deviceWidth > TABLET_SCREEN_WIDTH
-            ? DIMENSION_MODES.IS_TABLET_PORTRAIT_MODE
-            : deviceWidth > SMALL_SCREEN_WIDTH
-                ? DIMENSION_MODES.IS_SMALL_SCREEN_MODE
-                : DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
+    if (isLandscapeDevice) {
+        return DIMENSION_MODES.IS_TABLET_LANDSCAPE_MODE;
+    }
+    if (deviceWidth > TABLET_SCREEN_WIDTH) {
+        return DIMENSION_MODES.IS_TABLET_PORTRAIT_MODE;
+    }
+    if (deviceWidth > SMALL_SCREEN_WIDTH) {
+        return DIMENSION_MODES.IS_SMALL_SCREEN_MODE;
+    }
+    return DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
 };
 
 // Determine the dimension mode of the screen based on window dimensions
 export const getDimensionModeOfScreen = (window) => {
-    if (window?.width && window?.height) {
-        const {width: screenWidth, height: screenHeight} = window;
-
-        switch (true) {
-            case screenWidth > LARGE_SCREEN_WIDTH && screenWidth > screenHeight:
-                return DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
-
-            case screenWidth >= TABLET_SCREEN_WIDTH &&
-            screenWidth <= LARGE_SCREEN_WIDTH &&
-            screenWidth > screenHeight:
-                return DIMENSION_MODES.IS_TABLET_LANDSCAPE_MODE;
-
-            case screenWidth >= SMALL_SCREEN_WIDTH &&
-            screenWidth < TABLET_SCREEN_WIDTH &&
-            screenWidth < screenHeight:
-                return DIMENSION_MODES.IS_TABLET_PORTRAIT_MODE;
-
-            case screenWidth < SMALL_SCREEN_WIDTH && screenWidth < screenHeight:
-                return DIMENSION_MODES.IS_SMALL_SCREEN_MODE;
-
-            default:
-                return DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
-        }
-    } else {
+    if (!window || !window.width || !window.height) {
         // If window dimensions are not available, assume large screen mode
         return DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
     }
+
+    const {width: screenWidth, height: screenHeight} = window;
+
+    if (screenWidth > LARGE_SCREEN_WIDTH &&
+        screenWidth > screenHeight) {
+        return DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
+    }
+
+    if (screenWidth >= TABLET_SCREEN_WIDTH &&
+        screenWidth <= LARGE_SCREEN_WIDTH &&
+        screenWidth > screenHeight) {
+        return DIMENSION_MODES.IS_TABLET_LANDSCAPE_MODE;
+    }
+
+    if (screenWidth >= SMALL_SCREEN_WIDTH &&
+        screenWidth < TABLET_SCREEN_WIDTH &&
+        screenWidth < screenHeight) {
+        return DIMENSION_MODES.IS_TABLET_PORTRAIT_MODE;
+    }
+
+    if (screenWidth < SMALL_SCREEN_WIDTH && screenWidth < screenHeight) {
+        return DIMENSION_MODES.IS_SMALL_SCREEN_MODE;
+    }
+
+    // Default to large screen mode if no conditions are met
+    return DIMENSION_MODES.IS_LARGE_SCREEN_MODE;
 };
 
 export const isLandscapeMode = (context) => {
     if (!isWeb) {
         return isLandscapeDevice;
-    } else {
-        const isLargeScreenMode = context?.isLargeScreenMode || false;
-        const isTabletLandscapeMode = context?.isTabletLandscapeMode || false;
-        return isLargeScreenMode || isTabletLandscapeMode;
     }
+
+    const isLargeScreenMode = context?.isLargeScreenMode || false;
+    const isTabletLandscapeMode = context?.isTabletLandscapeMode || false;
+
+    return isLargeScreenMode || isTabletLandscapeMode;
 };
