ddc3e934b968803da2f39bc90a75d5a5b857b845
Hemanth-T2S <hemanth.naidu@foodhub.com>
2023-09-14 18:41:21 +0530

    styles moved to helper

--- src/MainModule/Styles/dashboardHeaderStyles.js
+++ src/MainModule/Styles/dashboardHeaderStyles.js
@@ -1,27 +1,27 @@
-import {StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 
 export const dashboardHeaderStyle = StyleSheet.create({
-    headerContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        borderBottomWidth: 1,
-        borderBottomColor: "#ccc",
-        backgroundColor: "#fff",
-        borderRadius: 1,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        elevation: 3,
-        marginBottom: 16,
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    backgroundColor: "#fff",
+    borderRadius: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    header: {
-        fontSize: 24,
-        fontWeight: "bold",
-        padding: 16,
-    }
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 3,
+    marginBottom: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 16,
+  },
 });
