9c1a77727440f891d72b6934529babfe8a3d4131
Hemanth-T2S <hemanth.naidu@foodhub.com>
2023-09-14 18:35:25 +0530

    styles moved to helper

--- src/MainModule/Styles/dashboardStyles.tsx
+++ src/MainModule/Styles/dashboardStyles.tsx
@@ -1,53 +1,55 @@
 // @ts-ignore
 export const dashboardStyles = {
-    mobileStyle: {
-        container: {
-            flex: 1,
-            backgroundColor: "#fff",
-        },
-        centerContainer: {
-            flex: 1,
-            marginTop: 30,
-        },
-        filterContainer: {
-            flex: 0.25,
-            height: "100%",
-        },
-        moduleContainer: {
-            flexDirection: "column",
-            marginBottom: 10,
-        },
-        secondaryContainer: {
-            flex: 1,
-            marginTop: 50,
-        },
-        module: {
-            flex: 1,
-            alignItems: "center",
-            marginBottom: 50,
-        },
-        icon: {
-            width: 150,
-            height: 150,
-            marginBottom: 8,
-        },
-        moduleName: {
-            fontSize: 16,
-        },
+  mobileStyle: {
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      marginTop: 30,
     },
-    landScapeStyle: {
-        centerContainer: {
-            flex: 1,
-            flexDirection: "row",
-        },
-        moduleContainer: {
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: 10,
-        },
-        module: {
-            flex: 1,
-            alignItems: "center",
-        },
+    centerContainer: {
+      flex: 1,
+      marginTop: 30,
     },
+    filterContainer: {
+      flex: 0.25,
+      height: "100%",
+    },
+    moduleContainer: {
+      flexDirection: "column",
+      marginBottom: 10,
+    },
+    secondaryContainer: {
+      flex: 1,
+      marginTop: 50,
+    },
+    module: {
+      flex: 1,
+      alignItems: "center",
+      marginBottom: 50,
+    },
+    icon: {
+      width: 150,
+      height: 150,
+      marginBottom: 8,
+    },
+    moduleName: {
+      fontSize: 16,
+    },
+  },
+  landScapeStyle: {
+    centerContainer: {
+      flex: 1,
+      flexDirection: "row",
+      marginTop: 50,
+    },
+    moduleContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginBottom: 10,
+    },
+    module: {
+      flex: 1,
+      alignItems: "center",
+    },
+  },
 };
